d0efffa5ae999724a25d5ea7cbfe7b2fd31c6087
Ebalo <12586380+ebalo55@users.noreply.github.com>
2025-04-18 11:50:05 +0200

    chore: null update

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -8,14 +8,21 @@
 import dayjs from "dayjs";
 import advanced_format from "dayjs/plugin/advancedFormat";
 import duration_plugin from "dayjs/plugin/duration";
+import relative_time from "dayjs/plugin/relativeTime";
 import utc from "dayjs/plugin/utc";
+import week_of_year from "dayjs/plugin/weekOfYear";
 
 dayjs.extend(utc);
 dayjs.extend(duration_plugin);
 dayjs.extend(advanced_format);
+dayjs.extend(relative_time);
+dayjs.extend(week_of_year);
 
 type Dayjs = dayjs.Dayjs;
 export { dayjs, type Dayjs };
 
 export type utc = typeof utc;
 export type { Duration, DurationUnitsObjectType, DurationUnitType, CreateDurationType } from "dayjs/plugin/duration";
+export type advanced_format = typeof advanced_format;
+export type relative_time = typeof relative_time;
+export type week_of_year = typeof week_of_year;
